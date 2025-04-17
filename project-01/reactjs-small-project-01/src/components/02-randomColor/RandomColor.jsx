020ad96cdbb420ac69e890b50492664d361d0b7f
YashdeepN <yashdeepneema@hotmail.com>
2025-04-16 22:13:11 -0500

    buttons to change rgb and hex

--- project-01/reactjs-small-project-01/src/components/02-randomColor/RandomColor.jsx
+++ project-01/reactjs-small-project-01/src/components/02-randomColor/RandomColor.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import "./style.css";
 
 const RandomColor = () => {
+  const [type, setType] = useState("hex");
+  const [color, setColor] = useState("#124536");
+
+  function someFunc() {}
+
   return (
-    <div className="container">
-      <button>Generate Random Color</button>
+    <div
+      className="container"
+      style={{
+        background: color,
+      }}
+    >
+      <button onClick={() => someFunc}>Generate Random Color</button>
+      <button onClick={() => setType("hex")}>create HEX Color</button>
+      <button onClick={() => setType("rgb")}>create RGB Color</button>
     </div>
   );
 };
