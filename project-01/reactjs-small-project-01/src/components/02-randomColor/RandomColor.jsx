25a032ca8a856d8a9e9d96c32cfd941900d4c9b3
YashdeepN <yashdeepneema@hotmail.com>
2025-04-17 22:21:46 -0500

    implement random color with both hex and rgb scheme

--- project-01/reactjs-small-project-01/src/components/02-randomColor/RandomColor.jsx
+++ project-01/reactjs-small-project-01/src/components/02-randomColor/RandomColor.jsx
@@ -2,21 +2,71 @@ import React, { useState } from "react";
 import "./style.css";
 
 const RandomColor = () => {
-  const [type, setType] = useState("hex");
-  const [color, setColor] = useState("#124536");
+  const [colorType, setColorType] = useState("hex");
+  const [bgcolor, setbgColor] = useState("#124536");
 
-  function someFunc() {}
+  function getRandomValue(num) {
+    return Math.floor(Math.random() * num);
+  }
+
+  function generateHex() {
+    const hexValues = [
+      0,
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+    ];
+    let hexColor = "#";
+    const HEX_LENGTH = hexValues.length;
+
+    for (let i = 0; i < 6; ++i) {
+      hexColor += hexValues[getRandomValue(HEX_LENGTH)];
+    }
+    setbgColor(hexColor);
+  }
+
+  function generateRGB() {
+    const [r, g, b] = [
+      getRandomValue(255),
+      getRandomValue(255),
+      getRandomValue(255),
+    ];
+    let rgbColor = `rgb(${r}, ${g}, ${b})`;
+    setbgColor(rgbColor);
+  }
+
+  function someFunc() {
+    if (colorType === "hex") generateHex();
+    else generateRGB();
+  }
 
   return (
     <div
       className="container"
       style={{
-        background: color,
+        background: bgcolor,
       }}
     >
-      <button onClick={() => someFunc}>Generate Random Color</button>
-      <button onClick={() => setType("hex")}>create HEX Color</button>
-      <button onClick={() => setType("rgb")}>create RGB Color</button>
+      <button onClick={someFunc}>Generate Random Color</button>
+      <button onClick={() => setColorType("hex")}>create HEX Color</button>
+      <button onClick={() => setColorType("rgb")}>create RGB Color</button>
+
+      <div>
+        <h2>{colorType === "hex" ? "HEX Color" : "RGB Color"}</h2>
+        <h2>{bgcolor}</h2>
+      </div>
     </div>
   );
 };
