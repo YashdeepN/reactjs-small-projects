91d983416a3a1094b5318a80cdae0982c68a4431
YashdeepN <yashdeepneema@hotmail.com>
2025-04-29 22:32:55 -0500

    add browse jobs section

--- project-02/reactjs-jobs/src/App.jsx
+++ project-02/reactjs-jobs/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import NavBar from "./Components/NavBar";
+import Hero from "./Components/Hero";
+import HomeCards from "./Components/HomeCards";
+import JobListings from "./Components/JobListings";
 
 const App = () => {
-  return <div className="text-5xl">App</div>;
+  return (
+    <>
+      <NavBar />
+      <Hero
+        title="Become a React Dev"
+        subtitle="Find the React job that fits your skills and needs"
+      />
+      <HomeCards />
+      <JobListings />
+    </>
+  );
 };
 
 export default App;
