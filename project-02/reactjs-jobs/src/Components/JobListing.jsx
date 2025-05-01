0753c5399c49c5af83670ed4c682a04bad5b62ee
YashdeepN <yashdeepneema@hotmail.com>
2025-05-01 07:13:52 -0500

    fix more on job listing description using prevState

--- project-02/reactjs-jobs/src/Components/JobListing.jsx
+++ project-02/reactjs-jobs/src/Components/JobListing.jsx
@@ -18,7 +18,7 @@ const JobListing = ({ job }) => {
         <div className="mb-5">{description}</div>
 
         <button
-          onClick={() => setShowFullDescription(!showFullDescription)}
+          onClick={() => setShowFullDescription((prevState) => !prevState)}
           className="text-indigo-500 mb-5 hover:text-indigo-600"
         >
           {showFullDescription ? "Less" : "More"}
