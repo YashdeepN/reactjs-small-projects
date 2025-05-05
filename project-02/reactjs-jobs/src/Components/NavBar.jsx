a18813a700eeee0cef926a744fb3b330a88d8520
YashdeepN <yashdeepneema@hotmail.com>
2025-05-05 00:18:50 -0500

    implement isActive class in Navbar using NavLink

--- project-02/reactjs-jobs/src/Components/NavBar.jsx
+++ project-02/reactjs-jobs/src/Components/NavBar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import logo from "../assets/images/logo.png";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+  const linkClass = ({ isActive }) =>
+    isActive
+      ? "text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+      : "text-white  hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
+
   return (
     <>
       <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -17,24 +22,15 @@ const NavBar = () => {
               </a>
               <div className="md:ml-auto">
                 <div className="flex space-x-2">
-                  <Link
-                    to="/"
-                    className="text-white bg-blck hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
+                  <NavLink to="/" className={linkClass}>
                     Home
-                  </Link>
-                  <Link
-                    to="/jobs"
-                    className="text-white bg-blck hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
+                  </NavLink>
+                  <NavLink to="/jobs" className={linkClass}>
                     Jobs
-                  </Link>
-                  <Link
-                    to="/add-job"
-                    className="text-white bg-blck hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
-                  >
+                  </NavLink>
+                  <NavLink to="/add-job" className={linkClass}>
                     Add Job
-                  </Link>
+                  </NavLink>
                 </div>
               </div>
             </div>
